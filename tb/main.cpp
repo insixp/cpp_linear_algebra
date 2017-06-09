c6ee18b39ce8cdc4174177f8a6f05ab0b6283214
Tal Gonen <talgonen16@gmail.com>
2017-06-09 11:02:36 +0300

    Task changed better interface

--- tb/main.cpp
+++ tb/main.cpp
@@ -8,7 +8,7 @@ int main(){
 
     MATRIX<float>   my_matrix(2, 5);
     //my_matrix.print_matrix();
-    matrix_propetries matrix_prop = {2, 3};
+    matrix_propetries matrix_prop = {5, 3};
     matrix_index matrix_in = {0, 0};
     MATRIX<float> res_matrix    = my_matrix.get_sub_matrix(matrix_prop, matrix_in);
     res_matrix.print_matrix();
